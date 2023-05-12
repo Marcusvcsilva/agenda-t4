1f05c7b661edae34026ba312fd7d4e4d87772872
Marcusvcsilva <marcusv.cordeirosilva@gmail.com>
2023-05-11 22:20:37 -0300

    Update Contato.class.php

--- classes/Contato.class.php
+++ classes/Contato.class.php
@@ -44,10 +44,24 @@ class Contato{
     }
 
     public function Atualizar(){
-
+        $banco = Banco::conectar();
+        $sql = "UPDATE contatos SET nome = ?, email = ?, telefone = ? WHERE id = ?";
+        $banco->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $comando = $banco->prepare($sql);
+        $comando->execute(array($this->nome, $this->email, $this->telefone, $this->id));
+        Banco::desconectar();
+        // Retornar quantidade de linhas apagadas:
+        return $comando->rowCount();
     }
     public function BuscarPorID(){
-
+        $banco = Banco::conectar();
+        $sql = "SELECT * FROM contatos WHERE id = ?";
+        $comando = $banco->prepare($sql);
+        $comando->execute(array($this->id));
+        // "Salvar" o resultado da consulta (tabela) na $resultado
+        $resultado = $comando->fetchAll(PDO::FETCH_ASSOC);
+        Banco::desconectar();
+        return $resultado;
     }
 
 }
