4802bee63b7340f60cf18357b64eb18044ad5510
Marcusvcsilva <marcusv.cordeirosilva@gmail.com>
2023-05-17 22:16:56 -0300

    Update agenda.php

--- agenda.php
+++ agenda.php
@@ -23,6 +23,14 @@ $resultado = $c->Listar();
 </head>
 <body>
   <div class="container">
+
+  <div class="row justify-content-end">
+    <div class="col-1">
+      <a type="submit" href="sair.php" class="btn btn-danger">Sair</a>
+    </div>
+  </div>
+  
+    <h3>Bem vindo(a) <?=$_SESSION['dados']['nome']; ?></h3>
     <h1>Cadastro de contatos</h1>
     <form class="form-group" action="actions/cadastrar_contato.php" method="POST">
       <label for="nome">Nome completo:</label>
@@ -69,6 +77,6 @@ $resultado = $c->Listar();
 
   <script src = "https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
   <script src = "https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-  
+  <?php require_once('alertas.inc.php'); ?>
 </body>
 </html>
